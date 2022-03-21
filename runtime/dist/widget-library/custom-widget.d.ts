2117093b7858deae070f8c2c88814675ac7a1464
Meer Arfath (MEEA) <meea@softwareag.com>
2022-03-21 16:43:51 +0530

    updated gitignore

--- runtime/dist/widget-library/custom-widget.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { ColorPaletteComponent as ɵd } from './lib/color-picker/color-palette/color-palette-component';
-export { ColorPickerComponent as ɵb } from './lib/color-picker/color-picker-component';
-export { ColorSliderComponent as ɵc } from './lib/color-picker/color-slider/color-slider-component';
-export { GpKpiOverviewWidgetConfigComponent as ɵa } from './lib/gp-kpi-overview-widget-config/gp-kpi-overview-widget-config.component';
-export { previewImage as ɵe } from './lib/preview-image';
